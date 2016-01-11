d603e3421475a1700dff39f3d2446921904476ab
Tiffany <tar3820@gmail.com>
2016-01-11 15:32:35 -0800

    Added String and Sequence practice

---
+++ practice.py
@@ -0,0 +1,17 @@
+def main():
+   user_input = input("Please provide a word: ")
+
+   middle_3(user_input)
+   loop(user_input)
+
+def middle_3(the_string):
+   mid = len(the_string)//2
+   print(the_string[mid-1:mid+2])
+
+def loop(the_sequence):
+   print(the_sequence)
+   for x in the_sequence:
+      print(x)
+
+
+main()
